d29adc4cce5d31174759be7e1e2573bd3d2bce6e
Jenna-Smith <jennawsmith@outlook.com>
2017-07-28 11:26:58 -0400

    add custom jquery for hovers over h1 and h2

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-$.backstretch("../img/background.jpg"
+//Jquery Backstretch
+$.backstretch('../img/background.jpg');
+
+
+$('h1').hover(function () {
+    $(this).css('color', '#cc6666');
+}, function () {
+    $(this).css('color', '#184580');
+});
+
+$('h2').hover(function () {
+    $(this).css('color', '#184580');
+}, function () {
+    $(this).css('color', '#cc6666');
+});
